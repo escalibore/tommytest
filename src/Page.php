a692b75d36b56d3ec659bd8805927e6683f33fee
escalibore <tom@tberthold.com>
2015-12-21 11:01:33 -0600

    go

--- src/Page.php
+++ src/Page.php
@@ -1,9 +1,21 @@
 <?php
-namespace mysqlgrid;
+namespace mysqlgridspace;
 
 class Page {
     public function __construct()
     {
-        echo "hello, i am a page.";
+        echo "hello, i am a page.<br>";
     }
 }
+
+class Book {
+    public function __construct()
+    {
+        echo "hello, i am a book.<br>";
+    }
+}
+
+function lego() {
+    echo 'You are now a master builder.';
+}
+
